4c427d3e36ca6cf19d8472972171acb275a77def
zio0225 <your_email@example.com>
2026-07-03 17:09:10 +0900

    setting: router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./App.css";
+import { RouterProvider } from 'react-router-dom';
+import { Router } from './routes/router';
+import './App.css';
 
 function App() {
-  return <></>;
+  return <RouterProvider router={Router} />;
 }
 
 export default App;
