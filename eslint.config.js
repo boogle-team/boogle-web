909bbc9999a0aea314072c7ac299060e7b718db9
zio0225 <your_email@example.com>
2026-07-03 16:50:04 +0900

    setting: eslint 및 prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,33 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-explicit-any': 'warn',
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-console': 'warn',
     },
   },
 ])
