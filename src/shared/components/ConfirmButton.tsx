35fb21fa089f180de60b07cb89db15c78f2a2dd2
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-07-08 02:46:12 +0900

    style: 확인 버튼 px값 수정 (#25)

--- src/shared/components/ConfirmButton.tsx
+++ src/shared/components/ConfirmButton.tsx
@@ -4,13 +4,17 @@ interface ConfirmButtonPropTypes {
   disabled?: boolean;
 }
 
-const ConfirmButton = ({ label, onClick, disabled }: ConfirmButtonPropTypes) => {
+const ConfirmButton = ({
+  label,
+  onClick,
+  disabled,
+}: ConfirmButtonPropTypes) => {
   return (
     <button
       type="button"
       onClick={onClick}
       disabled={disabled}
-      className="caption shrink-0 rounded-sm bg-orange-2 px-3 py-1.5 text-orange-7 disabled:opacity-50"
+      className="caption shrink-0 rounded-sm bg-orange-2 px-4 py-1.5 text-orange-7 disabled:opacity-50"
     >
       {label}
     </button>
