932c04831fc8e94ff216207d6fb82e931254604d
Gon <zx1417@naver.com>
2026-07-07 20:03:08 +0900

    refactor: TopNavigation 리뷰 반영

--- src/shared/components/topNavigation/DefaultTopNavigation.tsx
+++ src/shared/components/topNavigation/DefaultTopNavigation.tsx
@@ -19,18 +19,18 @@ const DefaultTopNavigation = ({
     <header
       className={`relative h-[3.5625rem] w-full border-b border-gray-3 bg-beige-1 ${className}`}
     >
-      <button
-        type="button"
-        aria-label="뒤로가기"
-        onClick={handleBackButtonClick}
-        className="absolute left-layout top-1/2 flex h-10 w-10 -translate-y-1/2 items-center justify-center"
-      >
+        <button
+            type="button"
+            aria-label="뒤로가기"
+            onClick={handleBackButtonClick}
+            className="absolute left-layout top-1/2 flex h-10 w-10 -translate-y-1/2 items-center justify-center"
+        >
         <ChevronLeft
           aria-hidden="true"
           className="h-6 w-6 text-gray-8"
           strokeWidth={2}
         />
-      </button>
+        </button>
 
       <h1 className="body-m absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap text-center tracking-[-0.02rem] text-gray-10">
         {title}
