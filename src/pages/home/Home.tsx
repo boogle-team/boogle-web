ec9eb138ed949535b724ea397cad014190a6db2d
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-07-08 01:26:55 +0900

    feat: input text 컴포넌트 구현 (#25)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,45 @@
+import { useState } from 'react';
+
+import InputText from '@/shared/components/InputText';
+
 const Home = () => {
-  return <div></div>;
+  const [nickname, setNickname] = useState('');
+  const [filled, setFilled] = useState('땅콩잼');
+  const [tag, setTag] = useState('');
+
+  return (
+    <div className="flex flex-col gap-6 p-4">
+      <InputText
+        value={nickname}
+        onChange={setNickname}
+        placeholder="닉네임을 입력해주세요"
+        maxCount={10}
+      />
+      <InputText value={filled} onChange={setFilled} maxCount={10} />
+      <InputText
+        value="땅콩잼"
+        onChange={() => {}}
+        maxCount={10}
+        isError
+      />
+      <InputText
+        value={tag}
+        onChange={setTag}
+        placeholder="태그를 입력해주세요"
+        actionLabel="확인"
+        onAction={() => {}}
+        helperText="최대 10자까지 입력할 수 있어요"
+      />
+      <InputText
+        value="땅콩땅콩잼땅콩잼잼"
+        onChange={() => {}}
+        actionLabel="확인"
+        onAction={() => {}}
+        isError
+        errorMessage="10자 이내로 입력해주세요"
+      />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/shared/components/ConfirmButton.tsx
@@ -0,0 +1,20 @@
+interface ConfirmButtonPropTypes {
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const ConfirmButton = ({ label, onClick, disabled }: ConfirmButtonPropTypes) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
+      className="caption shrink-0 rounded-sm bg-orange-2 px-3 py-1.5 text-orange-7 disabled:opacity-50"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default ConfirmButton;
