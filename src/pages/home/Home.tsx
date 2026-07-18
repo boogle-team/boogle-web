50b75d6ee39742cefa674a732f2ebc16d788261f
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-07-18 19:22:38 +0900

    remove: home test code 삭제 (#40)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,5 @@
-import TopNavigation from '@/shared/components/topNavigation/TopNavigation';
-
 const Home = () => {
-  return (
-    <div>
-      <TopNavigation
-        variant="detail"
-        title="부글 기록하기"
-        subTitle="6월 17일 (화)"
-      />
-      <TopNavigation
-        variant="detail"
-        title="부글 기록하기"
-        subTitle="6월 17일 (화)"
-        isDeleteButtonVisible
-      />
-      <TopNavigation title="가이드 상세" />
-      <TopNavigation title="가이드" isBackButtonVisible={false} />
-      <TopNavigation title="가이드" isBorderVisible={false} />
-      <TopNavigation
-        title="가이드"
-        isBackButtonVisible={false}
-        isBorderVisible={false}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
