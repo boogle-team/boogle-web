4366a5bbda1140ab5b2400f5fd2470e92e72b480
Sohoo122 <sooho040@naver.com>
2026-07-20 19:09:03 +0900

    feat: useForm 작성

---
+++ src/pages/record/hooks/useRecordForm.ts
@@ -0,0 +1,61 @@
+import { useMemo, useState } from 'react';
+
+import type {
+  BowelStatusTypes,
+  FeelingTypes,
+  RecordFormStateTypes,
+  RecordTimeValueTypes,
+  StoolTypeId,
+} from '../types/recordTypes';
+
+const INITIAL_RECORD_FORM_STATE: RecordFormStateTypes = {
+  bowelStatus: 'yes',
+  time: { hour: 8, minute: 0, meridiem: 'PM' },
+  stoolType: null,
+  feeling: 'comfortable',
+  painLevel: 0,
+};
+
+export const useRecordForm = () => {
+  const [formState, setFormState] = useState<RecordFormStateTypes>(
+    INITIAL_RECORD_FORM_STATE,
+  );
+
+  const handleBowelStatusChange = (bowelStatus: BowelStatusTypes) => {
+    setFormState((prev) => ({ ...prev, bowelStatus }));
+  };
+
+  const handleTimeChange = (time: RecordTimeValueTypes) => {
+    setFormState((prev) => ({ ...prev, time }));
+  };
+
+  const handleStoolTypeChange = (stoolType: StoolTypeId) => {
+    setFormState((prev) => ({ ...prev, stoolType }));
+  };
+
+  const handleFeelingChange = (feeling: FeelingTypes) => {
+    setFormState((prev) => ({ ...prev, feeling }));
+  };
+
+  const handlePainLevelChange = (painLevel: number) => {
+    setFormState((prev) => ({ ...prev, painLevel }));
+  };
+
+  const isSubmittable = useMemo(() => {
+    if (formState.bowelStatus === 'no') {
+      return true;
+    }
+
+    return formState.stoolType !== null;
+  }, [formState.bowelStatus, formState.stoolType]);
+
+  return {
+    formState,
+    isSubmittable,
+    handleBowelStatusChange,
+    handleTimeChange,
+    handleStoolTypeChange,
+    handleFeelingChange,
+    handlePainLevelChange,
+  };
+};
