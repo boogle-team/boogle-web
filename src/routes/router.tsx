bee2a99e4486bc795c2c9791e6c711a2ba315932
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-07-21 05:08:11 +0900

    fix: vercel 배포 오류 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import MainLayout from '../layout/MainLayout';
-import Home from '../pages/home/Home';
-import Login from '../pages/login/Login';
-import Guide from '../pages/guide/Guide';
-import Report from '../pages/report/Report';
-import Calendar from '../pages/calendar/Calendar';
 import Notification from '@/pages/notification/Notification';
 import Settings from '@/pages/settings/Settings';
 import ProfileEdit from '../pages/settings/ProfileEdit';
