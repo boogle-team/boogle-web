79d4be574ee6fc34a91d24b7f1c375daeadcd6d1
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-07-21 02:27:07 +0900

    feat: 기록 작성 및 수정 임시 라우트 추가 (#46)

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
+
 import MainLayout from '../layout/MainLayout';
-import Home from '../pages/home/Home';
-import Login from '../pages/login/Login';
-import Guide from '../pages/guide/Guide';
-import Report from '../pages/report/Report';
+import BoogleRecordFormPage from '../pages/boogleRecord/BoogleRecordFormPage';
 import Calendar from '../pages/calendar/Calendar';
+import Guide from '../pages/guide/Guide';
+import Home from '../pages/home/Home';
+import LifeRecordFormPage from '../pages/lifeRecord/LifeRecordFormPage';
+import Login from '../pages/login/Login';
+import Report from '../pages/report/Report';
 
 export const Router = createBrowserRouter([
   {
@@ -18,6 +21,23 @@ export const Router = createBrowserRouter([
       {
         path: 'calendar',
         element: <Calendar />,
+      },
+      // TODO: 실제 부글/생활 기록 작성·수정 페이지 구현 후 임시 페이지 컴포넌트를 교체하면 됨.
+      {
+        path: 'boogle-record/new',
+        element: <BoogleRecordFormPage />,
+      },
+      {
+        path: 'boogle-record/edit/:recordId',
+        element: <BoogleRecordFormPage />,
+      },
+      {
+        path: 'life-record/new',
+        element: <LifeRecordFormPage />,
+      },
+      {
+        path: 'life-record/edit/:recordId',
+        element: <LifeRecordFormPage />,
       },
       {
         path: 'report',
