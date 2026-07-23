29b3d5aad99ad272be8a55676a3a3fbe2706655a
Sohoo122 <sooho040@naver.com>
2026-07-24 01:25:37 +0900

    feat: 음식 카드 component 제작

---
+++ src/pages/record/life/components/FoodChip.tsx
@@ -0,0 +1,48 @@
+import type { FunctionComponent, SVGProps } from 'react';
+
+interface FoodChipPropTypes {
+  label: string;
+  description: string;
+  Icon: FunctionComponent<SVGProps<SVGSVGElement>>;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const FoodChip = ({
+  label,
+  description,
+  Icon,
+  isSelected,
+  onClick,
+}: FoodChipPropTypes) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      aria-pressed={isSelected}
+      className={`flex w-[7.29169rem] flex-col items-start justify-between rounded-xl border bg-beige-1 px-4 py-2 transition-colors hover:not-disabled:cursor-pointer ${
+        isSelected ? 'border-orange-6 bg-orange-1' : 'border-gray-4'
+      }`}
+    >
+      <Icon className="h-13.5 w-13.5 shrink-0 self-center" aria-hidden="true" />
+
+      <span
+        className={`label-semi self-stretch text-center ${
+          isSelected ? 'text-orange-6' : 'text-gray-8'
+        }`}
+      >
+        {label}
+      </span>
+
+      <span
+        className={`caption self-stretch text-center ${
+          isSelected ? 'text-orange-4' : 'text-gray-7'
+        }`}
+      >
+        {description}
+      </span>
+    </button>
+  );
+};
+
+export default FoodChip;
