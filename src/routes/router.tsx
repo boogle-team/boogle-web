21d6cc42ec1e755cc40028e78fbf9f52861b782f
Sohoo122 <sooho040@naver.com>
2026-07-24 00:42:22 +0900

    fix : Delete unused import to fix build error

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import MainLayout from '../layout/MainLayout';
-import Home from '../pages/home/Home';
-import Login from '../pages/login/Login';
-import Guide from '../pages/guide/Guide';
-import Report from '../pages/report/Report';
-import Calendar from '../pages/calendar/Calendar';
 import Record from '../pages/record/main/Main';
 import RecordEdit from '../pages/record/edit/Edit';
 import RecordDetail from '../pages/record/detail/Detail';
