66aa8c30103d34fbdda31db567b2d2f744f937d6
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-07-25 02:47:57 +0900

    fix: 공통 태그 섹션 export 경로 수정 (#46)

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -6,7 +6,10 @@ export { default as SectionTitle } from './tagSection/SectionTitle';
 export type { SectionTitlePropTypes } from './tagSection/SectionTitle';
 export { default as Tag } from './tagSection/Tag';
 export type { TagPropTypes } from './tagSection/Tag';
-export { default as TagsSection } from './TagsSection';
-export type { TagsSectionPropTypes, TagItemTypes } from './TagsSection';
+export { default as TagsSection } from './tagSection/TagsSection';
+export type {
+  TagsSectionPropTypes,
+  TagItemTypes,
+} from './tagSection/TagsSection';
 export { default as TooltipPopover } from './TooltipPopover';
 export type { TooltipPopoverPropTypes } from './TooltipPopover';
