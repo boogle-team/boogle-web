5ab0ba01adca5cf9b46cf2adafa809f0d784df08
Sohoo122 <sooho040@naver.com>
2026-07-24 23:06:04 +0900

    chore: Edit icon styling

--- src/pages/record/main/components/DetailRecordLink.tsx
+++ src/pages/record/main/components/DetailRecordLink.tsx
@@ -15,8 +15,11 @@ const DetailRecordLink = ({
       onClick={onClick}
       className="flex w-full items-center gap-3 rounded-2xl border border-dashed border-orange-4 bg-orange-1/60 px-4 py-3 text-left"
     >
-      <span className="flex h-9 w-9 shrink-0 items-center justify-center rounded-full bg-orange-2 text-orange-6">
-        <SquarePen className="h-4 w-4" aria-hidden="true" />
+      <span className="flex h-10 w-10 shrink-0 items-center justify-center rounded-xl bg-orange-4 text-beige-1">
+        <SquarePen
+          className="h-[1.25038rem] w-[1.25038rem]"
+          aria-hidden="true"
+        />
       </span>
 
       <span className="flex min-w-0 flex-1 flex-col">
