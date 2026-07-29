22e6ae0f5848c3650807e0a32b98bb518dd1b86b
Sohoo122 <sooho040@naver.com>
2026-07-29 19:11:11 +0900

    fix: Hide topNavigation border

--- src/pages/calendar/Calendar.tsx
+++ src/pages/calendar/Calendar.tsx
@@ -112,6 +112,7 @@ const Calendar = () => {
       <TopNavigation
         title="캘린더"
         isBackButtonVisible={false}
+        isBorderVisible={false}
         className="bg-beige-5"
       />
 
