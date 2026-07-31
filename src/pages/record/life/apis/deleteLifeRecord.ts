f0fb413e0fd1747e25a9af771c7a23eddf268faa
zio0225 <your_email@example.com>
2026-07-31 17:57:06 +0900

    api: 생활기록 삭제 API 구현 #72

---
+++ src/pages/record/life/apis/deleteLifeRecord.ts
@@ -0,0 +1,11 @@
+import { api } from '@/shared/apis/axiosInstance';
+
+import type { LifeRecordApiSuccessResponseTypes } from '../types/lifeRecordApiTypes';
+
+export const deleteLifeRecord = async (lifeId: number) => {
+  const { data } = await api.delete<
+    LifeRecordApiSuccessResponseTypes<null>
+  >(`/api/v1/life-records/${lifeId}`);
+
+  return data.data;
+};
