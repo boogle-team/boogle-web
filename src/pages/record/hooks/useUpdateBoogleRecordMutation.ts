aeb6a94fb309eb842292d2a6ab7bcf309a3f3550
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-08-13 19:14:54 +0900

    refactor: 부글 기록에서 없음 예외 처리 QA 반영 (#124)

--- src/pages/record/hooks/useUpdateBoogleRecordMutation.ts
+++ src/pages/record/hooks/useUpdateBoogleRecordMutation.ts
@@ -1,25 +1,23 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { patchBoogleRecord } from '../apis/boogleRecordApis';
 import { invalidateRecordRelatedQueries } from './invalidateRecordQueries';
 import { getBoogleRecordQueryKey } from './useBoogleRecordQuery';
-
-import type { PatchBoogleRecordRequestTypes } from '../types/boogleRecordApiTypes';
-
-interface UpdateBoogleRecordMutationParamTypes {
-  recordId: number;
-  request: PatchBoogleRecordRequestTypes;
-}
+import { getDailyRecordQueryKey } from '@/shared/hooks/useDailyRecordQuery';
+import { updateBoogleRecord } from '../utils/boogleRecordMutationUtils';
 
 export const useUpdateBoogleRecordMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: ({ recordId, request }: UpdateBoogleRecordMutationParamTypes) =>
-      patchBoogleRecord(recordId, request),
-    onSuccess: (record) => {
+    mutationFn: updateBoogleRecord,
+    onSuccess: async (record) => {
       queryClient.setQueryData(getBoogleRecordQueryKey(record.id), record);
-      invalidateRecordRelatedQueries(queryClient);
+      queryClient.removeQueries({
+        queryKey: getDailyRecordQueryKey(record.regDate),
+        exact: true,
+      });
+
+      await invalidateRecordRelatedQueries(queryClient);
     },
   });
 };
