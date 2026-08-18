73da68a1405c843dd98c59901538b90b5d549a51
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-08-18 22:35:50 +0900

    style: home 생활 기록 블럭 아이콘 글자 위치 고정되어 이상해 보이지 않도록 수정 (#141)

--- src/shared/components/dailyRecord/LifeMetricCard.tsx
+++ src/shared/components/dailyRecord/LifeMetricCard.tsx
@@ -14,9 +14,11 @@ const LifeMetricCard = ({
   isWarning = false,
 }: LifeMetricCardPropTypes) => {
   return (
-    <section className="flex px-4 py-2 flex-col items-center justify-center rounded-xl bg-yellow-1 text-center">
+    <section className="grid grid-rows-[2rem_1fr_auto] justify-items-center rounded-xl bg-yellow-1 px-4 py-2 text-center">
       <span className="flex h-8 w-8 items-center justify-center">{icon}</span>
-      <span className="caption text-gray-7 mt-1">{label}</span>
+      <span className="caption mt-1 flex items-center text-gray-7">
+        {label}
+      </span>
       <span
         className={`label-semi ${
           isWarning ? 'text-semantic-danger' : 'text-gray-8'
