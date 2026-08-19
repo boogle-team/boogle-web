01887dbd99d5c20ca1a7f072f52fb8152ccb0e50
si yeon <90365472+siyeon9302@users.noreply.github.com>
2026-08-19 22:51:27 +0900

    feat: 리뷰 반영 - 가이드 상세 페이지로 이동 시에도 스크롤 초기화 (#141)

--- src/pages/guide/Guide.tsx
+++ src/pages/guide/Guide.tsx
@@ -1,9 +1,13 @@
 import GuideDetailContainer from '@/pages/guide/components/GuideDetailContainer';
 import GuideMainContainer from '@/pages/guide/components/GuideMainContainer';
+import useGuideScrollReset from '@/pages/guide/hooks/useGuideScrollReset';
 import { useGuideSelection } from '@/pages/guide/hooks/useGuideSelection';
 
 const Guide = () => {
   const { isInsufficient, selectedGuideId } = useGuideSelection();
+
+  useGuideScrollReset(selectedGuideId);
+
   if (!selectedGuideId) {
     return <GuideMainContainer isInsufficientPreview={isInsufficient} />;
   }
